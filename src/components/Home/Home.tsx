feea30be982d78f14e79418ab730992ebea4ae31
Amani Kiruga <amaniedu71@gmail.com>
2021-10-04 03:27:32 -0400

    Added styling to home page

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,28 +1,28 @@
-import {
-    TextField,
-    Button,
-    Typography,
-    Paper,
-    Box,
-    AppBar,
-    Toolbar,
-    IconButton,
-} from "@material-ui/core";
+import { Container, Typography, Button } from "@mui/material";
 import Nav from "../Nav/Nav";
 
 const Home = () => {
     return (
         <div>
             <Nav />
-            <h1>CISC275: Rewind</h1>
-            This is a simple website{" "}
-            <span>
-                <a href="/">Github Link</a>
-            </span>{" "}
-            that showcases projects of students in Dr. Bart's CISC275 class on
-            web development!
-            <h2> Current Students</h2>
-            <button>Add your projects</button>
+            <Container>
+                <Typography sx={{ mb: 2, mt: 2 }} variant="h2" component="h1">
+                    CISC275: Rewind
+                </Typography>
+
+                <Typography sx={{ mb: 3 }}>
+                    This is a simple website{" "}
+                    <span>
+                        <a href="/">Github Link</a>
+                    </span>{" "}
+                    that showcases projects of students in Dr. Bart's CISC275
+                    class on web development!
+                </Typography>
+                <Typography sx={{ mb: 1 }} variant="h4">
+                    Current Students
+                </Typography>
+                <Button> Add Your Project</Button>
+            </Container>
         </div>
     );
 };
--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,12 +1,27 @@
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import { Link } from "react-router-dom";
 const Nav = () => {
     return (
-        <div className="topnav">
-            <a className="active" href="#home">
-                Home
-            </a>
-            <a href="#projects">View Projects</a>
-            <button> Add Project</button>
-        </div>
+        <Box sx={{ flexGrow: 1 }}>
+            <AppBar position="static">
+                <Toolbar>
+                    <Typography
+                        variant="h6"
+                        component="div"
+                        sx={{ flexGrow: 1 }}
+                    >
+                        News
+                    </Typography>
+
+                    <Link to="/projects">View Projects</Link>
+                </Toolbar>
+            </AppBar>
+        </Box>
     );
 };
 
